c055627eb9cade66e70cad36598dcebd57ea8a77
aarav-10 <74184873+aarav-10@users.noreply.github.com>
2021-02-02 16:18:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
  
-  //level one
+  
   block1 = new Block(300,275,30,40);
   console.log(block1);
   block2 = new Block(330,275,30,40);
@@ -30,34 +30,32 @@ function setup() {
   block5 = new Block(420,275,30,40);
   block6 = new Block(450,275,30,40);
   block7 = new Block(480,275,30,40);
-  //level two
+  
   block8 = new Block(330,235,30,40);
   block9 = new Block(360,235,30,40);
   block10 = new Block(390,235,30,40);
   block11 = new Block(420,235,30,40);
   block12 = new Block(450,235,30,40);
-  //level three
+ 
   block13 = new Block(360,195,30,40);
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
-  //top
+  
   block16 = new Block(390,155,30,40);
 
-  //set 2 for second stand
-  //level one
   blocks1 = new Block(640,175,30,40);
   blocks2 = new Block(670,175,30,40);
   blocks3 = new Block(700,175,30,40);
   blocks4 = new Block(730,175,30,40);
   blocks5 = new Block(760,175,30,40);
-  //level two
+ 
   blocks6 = new Block(670,135,30,40);
   blocks7 = new Block(700,135,30,40);
   blocks8 = new Block(730,135,30,40);
-  //top
+ 
   blocks9 = new Block(700,95,30,40);
 
-  //ball holder with slings
+  
   ball = Bodies.circle(50,200,20);
   World.add(world,ball);
 
@@ -65,20 +63,19 @@ function setup() {
 
 }
 function draw() {
-  background(56,44,44); 
+  background("red"); 
  
-  //Engine.update(engine);
-  //text(mouseX + ',' + mouseY, 10, 15);
+ 
   textSize(20);
-  fill("lightyellow");
-  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
+  fill("yellow");
+  text("Drag hexagon and release it",100,30);
 
   ground.display();
   stand1.display();
   stand2.display();
   strokeWeight(2);
   stroke(15);
-  fill("skyblue");
+  fill("blue");
   block1.display();
   block2.display();
   block3.display();
@@ -92,23 +89,23 @@ function draw() {
   block10.display();
   block11.display();
   block12.display();
-  fill("turquoise");
+  fill("green");
   block13.display();
   block14.display();
   block15.display();
   fill("grey");
   block16.display();
-  fill("skyblue");
+  fill("blue");
   blocks1.display();
   blocks2.display();
   blocks3.display();
   blocks4.display();
   blocks5.display();
-  fill("grey");
+  fill("blue");
   blocks6.display();
   blocks7.display();
   blocks8.display();
-  fill("green")
+  fill("pink")
   blocks9.display();
   fill("gold");
   imageMode(CENTER)
